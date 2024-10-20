df3094bc0c02601b24fff2d2f4c8c178b9fa55ed
César Souza <cesarsouz10@gmail.com>
2024-10-20 14:21:03 -0300

    Add files via upload

--- src/componentes/ConteudoLateral.js
+++ src/componentes/ConteudoLateral.js
@@ -1,11 +1,11 @@
 import Lista from './Lista';
 import './Lista.css';
-import { FaHouse } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 import { IoMdSearch } from "react-icons/io";
 import { FiMessageCircle } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import { CgAddR } from "react-icons/cg";
-import { FaGear } from "react-icons/fa";
+import { PiGearFill } from "react-icons/pi";
 
 export default function ConteudoLateral() {
     return (
@@ -14,13 +14,13 @@ export default function ConteudoLateral() {
                 <ul className='menuLateral'>
                     <h1>Slim App</h1>
                     <div style={{ marginTop: '3vw' }}></div>
-                    <Lista texto='Inicio' icone={<FaHouse />} />
+                    <Lista texto='Inicio' icone={<FaHome />} />
                     <Lista texto='Pesquisar' icone={<IoMdSearch />} />
                     <Lista texto='Mensagem' icone={<FiMessageCircle />} />
                     <Lista texto='Notificações' icone={<FaRegHeart />} />
                     <Lista texto='Publicar' icone={<CgAddR />} />
                     <div style={{ marginTop: '18vw' }}></div>
-                    <Lista texto='Configurações' className='configu' icone={<FaGear />} />
+                    <Lista texto='Configurações' className='configu' icone={<PiGearFill />} />
                 </ul>
             </div>
         </div>
